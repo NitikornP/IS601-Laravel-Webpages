88bb8e63773ed86dd461a53f1e840d1c42e6d63d
nitikorn <np638@njit.edu>
2019-03-19 22:07:08 -0400

    Editing header og the website

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,16 +1,13 @@
+<head>
 
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Titi Learning">
-    <meta name="author" content="Ti">
 
-    <title>Titi Laravel</title>
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous">
+
+    <title>IS601 - Laravel</title>
     <link href="/css/jumbotron.css" rel="stylesheet">
-
-
+</head>
 
