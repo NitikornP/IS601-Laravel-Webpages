c2ae0ec7ebf4cd105cd362cf37480504d60c3fc7
nitikorn <np638@njit.edu>
2019-03-19 22:17:06 -0400

    Adding link for contact page

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,24 @@
 */
 
 Route::get('/', function () {
-    return view('pages.home');
+    return view('pages.index');
 });
 
 Route::get('/about', function () {
     return view('pages.about');
+});
+
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+
+Route::post('/contact', function () {
+
+   $data = request()->all();
+
+   echo "Email: " . $data['email'] . '<br>';
+   echo "Message: " .$data['body'];
+
+
 });
