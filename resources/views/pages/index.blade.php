8c81c0eed8352efa4fc498c656c66f8583711704
nitikorn <np638@njit.edu>
2019-03-19 22:13:17 -0400

    Adding content to homepage

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+                <div class="jumbotron">
+                        <div class="container">
+
+                                <h1 class="display-1">Have you ever tasted thai food?</h1>
+                                <h2 class="display-3">Do you know it's different between each region!</h2>
+
+                                <p>Thai food varies from region to region due to geography and history. That's why travelling in Thailand is a culinary adventure of discovery.</p>
+                                <p><a class="btn btn-primary btn-lg" href="https://www.templeofthai.com/cooking/thai-cuisine.php" role="button">Learn more &raquo;</a></p>
+                        </div>
+                </div>
+
+                <div class="container">
+                        <!-- Example row of columns -->
+                        <div class="row">
+                                <div class="col-md-3">
+                                        <h2>Central Thailand</h2>
+                                        <p>Home of the famous floating river market west of Bangkok. Many great rivers converge in this delta, making it a fertile plain for rice growing.
+                                                In the Central Plains region, food is mainly hot, salty, sweet and sour. Steamed jasmine rice is served with different types of nam prik (chili dipping sauces) and soups.
+                                                Tom yum goong (shrimp soup with lemon grass) is a favorite dish. Prepared dishes usually contain many condiments and dried spices.</p>
+                                        <p><a class="btn btn-secondary" href="https://www.templeofthai.com/cooking/bangkok-cuisine.php" role="button">View details &raquo;</a></p>
+                                </div>
+                                <div class="col-md-3">
+                                        <h2>Northern Thailand</h2>
+                                        <p>In the North of Thailand, around Chiang Mai, the land is mostly jungle-covered mountains and valleys. Historically this area is the home of the Lanna Thais, whose former capital was Chiang Mai.
+                                                In the North, the traditional food is mild or hot, salty and sour, but rarely sweet.
+                                                Sticky rice (glutinous) is served with steamed vegetables, nam phrik oong, soups and Northern-style curries are common.
+                                                The North is also well-known for naem, a sour sausage made of fermented minced pork, wrapped and steamed in banana leaf.
+                                                Food is traditionally eaten at a small table and diners sit on straw mats around the table, eating communally. </p>
+                                        <p><a class="btn btn-secondary" href="https://www.templeofthai.com/cooking/lanna-cuisine.php" role="button">View details &raquo;</a></p>
+                                </div>
+                                <div class="col-md-3">
+                                        <h2>Northeastern Thailand</h2>
+                                        <p>Also known as "Isan Region", is a high plateau, with low rainfall and porous soil. The cuisine is simple and is traditionally eaten on a pa kao table made from bamboo or rattan.
+                                                Food in the Northeast is hot, salty and sour. Papaya salad (sohm tahm), sour chopped meat salad (koi), and sour minced chicken, beef, or pork salad (larb) are favorites.
+                                                Many condiments are used but not many dried spices in this region.
+                                                Meals are based on sticky rice and a strong paste called nam phrik pla ra (spicy dipping sauce of fermented fish and chilis) accompanied by raw wild and cultivated vegetables and herbs.</p>
+                                        <p><a class="btn btn-secondary" href="https://www.templeofthai.com/cooking/isan-cuisine.php" role="button">View details &raquo;</a></p>
+                                </div>
+                                <div class="col-md-3">
+                                        <h2>Southern Thailand</h2>
+                                        <p>In the South of Thailand there is an abundance of coconut trees and seafood. Food in the South is renowned for being strong in taste - very hot, salty and sour.
+                                                Spicy curries are eaten every day, especially coconut curry and sour curry. Generally Southerners eat more fish than meat, because they reside close to the sea.
+                                                The most common dipping sauce or paste is nam phrik kapee (also called nam chuke), eaten with raw vegetables and crispy fried fish.
+                                                This dipping sauce is prepared with the best quality shrimp paste (kapee or kuey), fresh chilies (prik kee noo), a bit of palm sugar and fresh lime juice.</p>
+                                        <p><a class="btn btn-secondary" href="https://www.templeofthai.com/cooking/southern-cuisine.php" role="button">View details &raquo;</a></p>
+                                </div>
+                        </div>
+
+                        <hr>
+
+                </div>
+@endsection
