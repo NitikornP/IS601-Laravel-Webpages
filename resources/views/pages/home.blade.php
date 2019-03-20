2588a14f6209a8fb832a3b3e231b7cafcb0947ea
nitikorn <np638@njit.edu>
2019-03-19 22:12:11 -0400

    Editing homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.default')
 @section('content')
 
-    <h1 class="text-center">Hello and welcome to my homepage</h1>
+    <h1 class="text-center">Hello and welcome to my contact page</h1>
+
 
 @stop
