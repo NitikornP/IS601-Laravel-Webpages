6ff746ba459d71d040b3c675c622f5f0120fae56
nitikorn <np638@njit.edu>
2019-03-19 22:02:11 -0400

    Adding footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
+<!-- FOOTER -->
 <footer class="container">
     <p class="float-right"><a href="#">Back to top</a></p>
-    <p>&copy; Titi, Inc. &middot; <a href="#">Privacy Terms</a></p>
-
+    <p>&copy; Titi 2019</p>
 </footer>
