43d211a726da376d696a1dbb352b4e5269b073e0
nitikorn <np638@njit.edu>
2019-03-23 21:15:16 -0400

    Editing contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -5,11 +5,18 @@
         <div class="card-title">
             <h1>Contact page </h1>
             <p class="lead">Please use this form to contact site owner</p>
+
             <div class="card-body">
-                <form action="/contact" method="post">
+                <form action="{{route('contact.store')}}" method="post">
+
                     @csrf
+
                     <div class="form-group">
-                        <label for="email">Email address</label>
+                        <label for="name">Name</label>
+                        <input name="name" type="text" class="form-control" id="Name" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
                         <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
                     </div>
                     <div class="form-group">
@@ -19,5 +26,6 @@
                     <button type="submit" class="btn btn-primary mb-2">Submit</button>
                 </form>
             </div>
+
         </div>
 @endsection
