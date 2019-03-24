e598bacc5e7ce3196f024461829e1d3ca636c6b3
nitikorn <np638@njit.edu>
2019-03-23 21:17:20 -0400

    Adding thank you page

---
+++ resources/views/pages/thankyou.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <main role="main" class="container">
+
+        <div class="starter-template">
+            <br>
+            <h1>Thank you for submitting: {{$name}}</h1>
+        </div>
+
+    </main>
+@endsection
